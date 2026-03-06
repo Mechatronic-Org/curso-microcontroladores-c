ac79b38f8c3eb40691b1ed7f5b1f8f3a32fbd70b
J35U590 <141761160+J35U590@users.noreply.github.com>
2026-03-06 17:25:59 -0600

    Update main.c

--- 03_practicas/practica_01_structs/main.c
+++ 03_practicas/practica_01_structs/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+struct Sensor {
+    int id;
+    float temperatura;
+};
+
+int main() {
+
+    struct Sensor s1;
+
+    s1.id = 10;
+    s1.temperatura = 26.5;
+
+    printf("ID: %d\n", s1.id);
+    printf("Temp: %.2f\n", s1.temperatura);
+
+}
 
